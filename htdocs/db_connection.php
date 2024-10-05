4aa764a23e80111a1aa6779e5a39f0ac886bf4a5
Jessie T. Fabiaña <fabiana.jessie@ici.edu.ph>
2024-10-05 09:14:19 +0000

    upload db_connection file

---
+++ htdocs/db_connection.php
@@ -0,0 +1,16 @@
+<?php
+    
+    $servername = "127.0.0.1";
+    $username = "mariadb";
+    $password = "mariadb";
+    $dbname = "mariadb";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }   else {
+        echo "Connected successfully";
+    }
+    
+?>
